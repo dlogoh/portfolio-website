a633739ba29f19e401f16e438df55e886bebbdb0
David Harris <87347646+dlogoh@users.noreply.github.com>
2022-10-27 21:27:33 -0400

    debugging

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,24 +3,24 @@ import React from "react";
 function Contact() {
   function validateForm() {
     var name = document.getElementById("name").value;
-    if (name == "") {
+    if (name === "") {
       document.querySelector(".status").innerHTML = "Name cannot be empty";
       return false;
     }
     var email = document.getElementById("email").value;
-    if (email == "") {
+    if (email === "") {
       document.querySelector(".status").innerHTML = "Email cannot be empty";
       return false;
     } else {
       var re =
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (!re.test(email)) {
         document.querySelector(".status").innerHTML = "Email format invalid";
         return false;
       }
     }
     var message = document.getElementById("message").value;
-    if (message == "") {
+    if (message === "") {
       document.querySelector(".status").innerHTML = "Message cannot be empty";
       return false;
     }
