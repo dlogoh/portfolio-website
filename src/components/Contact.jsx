7c9c375e4f2ffcacc0e50ff6d7824161c74e96e4
David Harris <87347646+dlogoh@users.noreply.github.com>
2022-10-27 21:08:19 -0400

    Added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,102 @@
 import React from "react";
 
 function Contact() {
+  function validateForm() {
+    var name = document.getElementById("name").value;
+    if (name == "") {
+      document.querySelector(".status").innerHTML = "Name cannot be empty";
+      return false;
+    }
+    var email = document.getElementById("email").value;
+    if (email == "") {
+      document.querySelector(".status").innerHTML = "Email cannot be empty";
+      return false;
+    } else {
+      var re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(email)) {
+        document.querySelector(".status").innerHTML = "Email format invalid";
+        return false;
+      }
+    }
+    var message = document.getElementById("message").value;
+    if (message == "") {
+      document.querySelector(".status").innerHTML = "Message cannot be empty";
+      return false;
+    }
+    document.querySelector(".status").innerHTML = "Sending...";
+  }
+
   return (
-    <section id="contact">
+    <section>
       <div className="container mb-5">
-        <h2 className="text-center py-5">Send Me An Email</h2>
-        <hr />
-        <h5 className="text-center">
-          If you have an questions, comments, or concerns, please send me an
-          email. If you are interested in hiring me, feel free to either email
-          me or contact me on LinkedIn
-        </h5>
-        <hr />
-        <h3 className="text-center">davidharris@dlogoh.com</h3>
+        <h2 className="text-center py-5">Let's Get In Touch</h2>
+        <form
+          id="contact-form"
+          name="contact-form"
+          action="POST"
+          data-netlify="true"
+        >
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="company" className="form-label">
+              Company
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="company"
+              name="company"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">
+              Message
+            </label>
+            <textarea
+              className="form-control"
+              id="message"
+              name="message"
+              rows="3"
+              required
+            ></textarea>
+          </div>
+          <div className="field">
+            <div data-netlify-recaptcha="true"></div>
+          </div>
+        </form>
+        <button
+          type="submit"
+          className="btn btn-primary mt-3 fw-bold"
+          onClick={validateForm}
+        >
+          Submit
+        </button>
+        <div className="status mt-3" id="status"></div>
       </div>
     </section>
   );
